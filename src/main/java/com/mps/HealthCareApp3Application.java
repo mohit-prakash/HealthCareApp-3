7dfa9b445afee4676018b5490a71be2b9b2df422
Mohit Prakash <m2450p@gmail.com>
2023-02-24 23:45:25 +0530

    Initial commit

---
+++ src/main/java/com/mps/HealthCareApp3Application.java
@@ -0,0 +1,13 @@
+package com.mps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareApp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mps/HealthCareApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
